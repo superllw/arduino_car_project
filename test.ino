461d9c721fe909d5ba55ada3c179074e9019ae07
superllw <1714006347@qq.com>
2021-12-09 01:23:51 +0800

    master

--- test.ino
+++ test.ino
@@ -1,8 +1,16 @@
 /*
-    //BUILT AT 
-    模式1:巡线
-    模式2.避障
-    模式3.跟随
+    IDE选择的是vscode + arduino
+    舵机如果接口是9，10的话，可以导入Servo.h库，声明舵机，可以利用其中的write函数进行舵机角度的转换
+
+
+*/
+/*
+    BUILT AT                       11.09
+    BUILT BY                       LLW
+
+    模式1:巡线      待测试
+    模式2.避障      待测试
+    模式3.跟随      正常
     //行驶功能      正常            11.13
     //舵机转向      正常            11.13
     //按钮功能      正常            11.16
@@ -11,11 +19,9 @@
     //整体调试
 */
 
-#include<Servo.h>
-#include<LiquidCrystal.h>
-#include<move.h>
-
-Car_move mycar;
+#include<Servo.h>                   //导入舵机库
+#include<LiquidCrystal.h>           //导入屏幕库
+#include"move.h"                   //导入移动库
 
 //定义运动状态
 int STOP = 0;
@@ -25,57 +31,62 @@ int LEFT = 3;
 int RIGHT = 4;
 
 //定义电机接口
-int Leftmotor1 = 5;             //左电机后退
-int Leftmotor2 = 9;             //左电机前进
-int Rightmotor1 = 6;            //右电机前进
-int Rightmotor2 = 10;           //右电机后退
+int Leftmotor1 = 5;                 //左电机后退
+int Leftmotor2 = 9;                 //左电机前进
+int Rightmotor1 = 6;                //右电机前进
+int Rightmotor2 = 10;               //右电机后退
 //定义舵机
 Servo Myservo;
-
-
+//声明led屏
+LiquidCrystal lcd(3,4,7,8,11,12,13);
+//声明小车移动
+Car_move mycar;
 
 //定义按钮接口
-int Button_1 = A0;              //翻页按钮
-int Button_2 = A1;              //确认按钮
+int Button_1 = A0;                  //翻页按钮
+int Button_2 = A1;                  //确认按钮
 //定义超声波接受，发送接口
 int Trig = A4;
 int Echo =A5;
-//声明led屏
-LiquidCrystal lcd(3,4,7,8,11,12,13);
-const int SensorRight = A2;   	//右循迹红外传感器(P3.2 OUT1)
-const int SensorLeft = A3;     	//左循迹红外传感器(P3.3 OUT2)
+//定义红外传感器
+const int SensorRight = A2;   	    //右循迹红外传感器(P3.2 OUT1)
+const int SensorLeft = A3;     	    //左循迹红外传感器(P3.3 OUT2)
 
-int SL;    //左循迹红外传感器状态
-int SR;    //右循迹红外传感器状态
+int Hongwai_Left;                   //左循迹红外传感器状态
+int Hongwai_Right;                  //右循迹红外传感器状态
 
-int servopin=2;//设置舵机驱动脚到数字口2
-int myangle;//定义角度变量
-int pulsewidth;//定义脉宽变量
-int val;
+int servopin = 2;                   //设置舵机驱动脚到数字口2
+int angle;                          //定义角度变量
+int pulsewidth;                     //定义脉宽变量
 
 
-int menu();                     //菜单
-int GetDistance();              //超声波获取距离
-void Car_main();                //小车主函数
-void Lcd_display(int distance);
-void car_tracking();
-void car_avoid();
-void car_follow();
-void goback(int v,int t);
-//--------------------------------------------------//
+int menu();                         //菜单
+int GetDistance();                  //超声波获取距离
+void Car_main();                    //小车主函数    
+void car_tracking();                //小车循迹
+void car_avoid();                   //小车避障
+void car_follow();                  //小车跟随
+
+//-------------------------------------------------------------------//
 void setup(){
-    pinMode(Leftmotor1,OUTPUT);
+    //小车电机端口设置为输出
+    pinMode(Leftmotor1,OUTPUT);     
     pinMode(Leftmotor2,OUTPUT);
     pinMode(Rightmotor1,OUTPUT);
     pinMode(Rightmotor2,OUTPUT);
-    pinMode(Button_1,INPUT);
+    //按钮1，2端口设置为输入
+    pinMode(Button_1,INPUT);        
     pinMode(Button_2,INPUT);
-    pinMode(Trig,OUTPUT);
-    pinMode(Echo,INPUT);
+
+    pinMode(Trig,OUTPUT);           //超声波发送端口设置为输出
+    pinMode(Echo,INPUT);            //超声波接受端口设置为输入
+    //红外线端口设置为输入
     pinMode(SensorRight, INPUT);    
     pinMode(SensorLeft, INPUT); 
-    pinMode(servopin,OUTPUT);//设定舵机接口为输出接口
-    // lcd.begin(16,2);
+    //设定舵机接口为输出接口
+    //如果采用的舵机接口为9跟10可以采用 Myservo.attach()设置舵机端口
+    pinMode(servopin,OUTPUT);       
+    
     //初始化串口
     Serial.begin(9600);
   
@@ -87,30 +98,31 @@ void loop(){
 
 }
 
-//-------------------------------------------------//
+//-----------------------------------------------------------------//
 
 int menu()
 {
-    int state = 0;
-    while(1)                   //确认按钮没被按下时
+    int state = 0;              //存放返回的值
+    while(1)                    //确认按钮没被按下时
     {
-        lcd.clear();
-        lcd.setCursor(4,1);
-        lcd.print("1.Tracking");
-        state++;
-        delay(200);
+        lcd.clear();            //清屏
+        lcd.setCursor(4,1);     //从第一行第四列开始写
+        lcd.print("1.Tracking");//输出文本
+        state++;                //返回值加一
+        delay(200);             //延时
+
         while(1)
         {
-            if(digitalRead(Button_1)||digitalRead(Button_2))
+            if(digitalRead(Button_1)||digitalRead(Button_2))        //按钮一或者按钮二按下，退出死循环
             {
                 break;
             }
         }
-        if(digitalRead(Button_2)==1)                   //确认1功能
+        if(digitalRead(Button_2)==1)                   //如果是按钮二按下，表示确认1功能
         {
-           goto zhao;
-        }else{
-            lcd.clear();
+           goto zhao;                                  //退出外层循环
+        }else{                                         //表示按下的是按钮一，进行翻页操作
+            lcd.clear();                               //同上
             lcd.setCursor(4,1);
             lcd.print("2.Avoid");
             state++;
@@ -124,7 +136,7 @@ int menu()
             }
             if(digitalRead(Button_2)==1)                  //确认2功能
             {
-               goto zhao;
+               goto zhao;                               //退出外层循环
             }else{
                 lcd.clear();
                 lcd.setCursor(4,1);
@@ -149,28 +161,37 @@ int menu()
         }
     }
     zhao:
-    return state;
+    return state;                                           //返回值，表示选择的模式
 }
 void car_tracking(){
-    digitalWrite(2,LOW);
+    
     while(1)
     {
-        SR = digitalRead(SensorRight);
-        SL = digitalRead(SensorLeft);
-        if (SL == LOW&&SR==LOW)
-            mycar.run(150,0);  
-        else if (SL == HIGH & SR == LOW)// 左循迹红外传感器,检测到信号
-        
-            mycar.turnleft(150,0);
-        else if (SR == HIGH & SL == LOW) // 右循迹红外传感器,检测到信号，车子向左偏离轨道，向右转  
-            mycar.turnright(150,0);
-        else // 都是白色, 停止
+        H = digitalRead(SensorRight);                      //读取右红外线传感器电平
+    Hhongwai_Left = digitalRead(SensorLeft);                       //读取左红外线传感器电平
+        if Hhongwai_Left == LOW&&H==LOW){                            //左右红外线都为低电平，表示都未检测到黑线
+            mycar.run(100,0);                               //小车直走 
+            delay(10);
+        } 
+        else if Hhongwai_Left == HIGH & H == LOW)                    //左红外线传感器为高电平，右红外线传感器为低电平，表示向右偏离
+        {
+            mycar.turnleft(80,0);                           //小车向左转
+            delay(10);
+        }
+        else if (H == HIGH &Hhongwai_Left == LOW)                    //右红外线传感器为高电平，左红外线传感器为低电平，表示向左偏离
+        {
+            mycar.turnright(80,0);                          //小车右转
+            delay(10);
+        }
+        else{                                               //都检测到黑线，小车停车
             mycar.brake(10);
+        }
+        delay(2);
     }
 }
 
 void car_avoid(){
-    int Distance[2];
+    int Distance[2];                                       //声明数组，存放距离
     //Distance[0]:  左边的距离，Distance[1]:中间的距离，Distance[2]：右边的距离
     while(1)
     {
@@ -210,7 +231,42 @@ void car_avoid(){
     }
 }
 
+
+
+void car_follow()
+{
+    for(int i;i<15;i++){
+            servopulse(80);                     //舵机摆正
+    }
+    while(1)
+    {
+        int distance = GetDistance();           //读取距离
+        if(distance < 10){
+            mycar.goback(100,1);                //小于10，后退
+            delay(10);
+        }else if(distance == 10){               
+            mycar.brake(1);                     //等于10，停止
+            delay(10);
+        }else if((distance > 10) && (distance < 30)){
+            mycar.run(100,1);                   //在10与30之间，前进
+            delay(10);
+        }else{
+            mycar.brake(1);                     //大于30，停止
+            delay(10);
+        }
+        // Serial.println(distance);            //串口通讯，返回距离值
+    }
+}
+
 int GetDistance(){
+    /*超声波测距函数，原理见博客：
+    https://blog.csdn.net/u012308586/article/details/105747760?ops_request_misc=%25
+    7B%2522request%255Fid%2522%253A%2522163897671216780261995044%2522%252C%2522scm%
+    2522%253A%252220140713.130102334..%2522%257D&request_id=16389767121678026199504
+    4&biz_id=0&utm_medium=distribute.pc_search_result.none-task-blog-2~all~top_posi
+    tive~default-2-105747760.first_rank_v2_pc_rank_v29&utm_term=arduino%E8%B6%85%E5
+    %A3%B0%E6%B3%A2%E6%B5%8B%E8%B7%9D&spm=1018.2226.3001.4187*/
+
     digitalWrite(Trig,LOW);
     delayMicroseconds(2);
     digitalWrite(Trig,HIGH);
@@ -221,35 +277,20 @@ int GetDistance(){
     return distance;
 }
 
-void car_follow()
+void servopulse(int angle)                        //舵机转动到angle角度
 {
-    for(int i;i<15;i++){
-            servopulse(80);
-    }
-    while(1)
-    {
-        int distance = GetDistance();
-        if(distance < 10){
-            mycar.goback(100,1);
-            delay(10);
-        }else if(distance == 10){
-            mycar.brake(1);
-            delay(10);
-        }else if((distance > 10) && (distance < 30)){
-            mycar.run(100,1);
-            delay(10);
-        }else{
-            mycar.brake(1);
-            delay(10);
-        }
-        // Serial.println(distance);
-    }
+    //原理就是模拟输出PWM值
+    pulsewidth=(angle*11)+500;                    //将角度转化为脉宽值
+    digitalWrite(servopin,HIGH);
+    delayMicroseconds(pulsewidth);                //延时脉宽值的微秒数
+    digitalWrite(servopin,LOW);
+    delay(20-pulsewidth/1000);                    ////延时周期内剩余时间
 }
 
 void Car_main()
 {
-    int choose = menu();
-    switch (choose)
+    int choose = menu();                        //进入主菜单，获取模式返回值
+    switch (choose)                             //按返回值进入不同的模式
     {
     case 1:
     //循迹功能：
@@ -273,27 +314,20 @@ void Car_main()
         lcd.print("Now is follow");
         car_follow();
         break;
-    default:
+    default:                                        //返回值有误报错
         lcd.clear();
         lcd.setCursor(4,1);
         lcd.print("error!");
+        while(1);
         break;
     }
     
 }
 
+
+
 void test()
 {
-    mycar.turnleft(200,3);          //90度左转
-    // mycar.turnright(200,3);         //90度右转
-    while(1);
+    //测试函数，用于调试小车部分功能
 }
 
-void servopulse(int myangle)
-{
-    pulsewidth=(myangle*11)+500;
-    digitalWrite(servopin,HIGH);
-    delayMicroseconds(pulsewidth);
-    digitalWrite(servopin,LOW);
-    delay(20-pulsewidth/1000);
-}
